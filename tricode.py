9e72be9618de2207c7e733fb48cb46e13d3013ff
sb565 <shivankbali.work@gmail.com>
2019-12-19 23:23:52 +0530

    Transpose Image Corrected

--- tricode.py
+++ tricode.py
@@ -11,9 +11,9 @@ def show_digits(digits, colour=255):
     rows = []
     with_border = [cv2.copyMakeBorder(img.copy(), 1, 1, 1, 1, cv2.BORDER_CONSTANT, None, colour) for img in digits]
     for i in range(9):
-        row = np.concatenate(with_border[i * 9:((i + 1) * 9)], axis=1)
+        row = np.concatenate(with_border[i * 9:((i + 1) * 9)], axis=0)
         rows.append(row)
-    show_image(np.concatenate(rows))
+    show_image(np.concatenate(rows,axis=1))
 
 def infer_grid(img):
 	"""Infers 81 cell grid from a square image."""
@@ -36,14 +36,9 @@ def distance_between(p1, p2):
 
 def crop_and_warp(img, crop_rect):
 	"""Crops and warps a rectangular section from an image into a square of similar size."""
-
-	# Rectangle described by top left, top right, bottom right and bottom left points
 	top_left, top_right, bottom_right, bottom_left = crop_rect[0], crop_rect[1], crop_rect[2], crop_rect[3]
-
-	# Explicitly set the data type to float32 or `getPerspectiveTransform` will throw an error
 	src = np.array([top_left, top_right, bottom_right, bottom_left], dtype='float32')
 
-	# Get the longest side in the rectangle
 	side = max([
 		distance_between(bottom_right, top_right),
 		distance_between(top_left, bottom_left),
@@ -51,13 +46,8 @@ def crop_and_warp(img, crop_rect):
 		distance_between(top_left, top_right)
 	])
 
-	# Describe a square with side of the calculated length, this is the new perspective we want to warp to
 	dst = np.array([[0, 0], [side - 1, 0], [side - 1, side - 1], [0, side - 1]], dtype='float32')
-
-	# Gets the transformation matrix for skewing the image to fit a square by comparing the 4 before and after points
 	m = cv2.getPerspectiveTransform(src, dst)
-
-	# Performs the transformation on the original image
 	return cv2.warpPerspective(img, m, (int(side), int(side)))
   
 
@@ -68,33 +58,18 @@ def find_corners_of_largest_polygon(img):
 	contours = sorted(contours, key=cv2.contourArea, reverse=True)  # Sort by area, descending
 	polygon = contours[0]  # Largest image
 
-	# Use of `operator.itemgetter` with `max` and `min` allows us to get the index of the point
-	# Each point is an array of 1 coordinate, hence the [0] getter, then [0] or [1] used to get x and y respectively.
-
-	# Bottom-right point has the largest (x + y) value
-	# Top-left has point smallest (x + y) value
-	# Bottom-left point has smallest (x - y) value
-	# Top-right point has largest (x - y) value
 	bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
 	top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
 	bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
 	top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-
-	# Return an array of all 4 points using the indices
-	# Each point is in its own array of one coordinate
 	return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
 
 def pre_process_image(img, skip_dilate=False):
 	"""Blurs, applies adaptive thresholding, inverts colour and dilates the image."""
-	# Blur to improve the efficacy of the thresholding and minimise the number of contours
 	proc = cv2.GaussianBlur(img.copy(), (9, 9), 0)
-
-	# Block size of 5 gives quite a harsh thresholding, but this is required to extract the outer grid from
-	# the inner grid from some photos
 	proc = cv2.adaptiveThreshold(proc, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 2)
 	proc = cv2.bitwise_not(proc, proc)
 
-	# Dilate the image to increase the size of the grid lines. Should increase reliability in finding the grid
 	if not skip_dilate:
 		kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]], np.uint8)
 		proc = cv2.dilate(proc, kernel)
@@ -201,19 +176,12 @@ def extract_digit(img, rect, size):
 	"""Extracts a digit (if one exists) from a Sudoku square."""
 
 	digit = cut_from_rect(img, rect)  # Get the digit box from the whole square
-
-	# Use fill feature finding to get the largest feature in middle of the box
-	# Margin used to define an area in the middle we would expect to find a pixel belonging to the digit
 	h, w = digit.shape[:2]
 	margin = int(np.mean([h, w]) / 2.5)
 	_, bbox, seed = find_largest_feature(digit, [margin, margin], [w - margin, h - margin])
 	digit = cut_from_rect(digit, bbox)
-
-	# Scale and pad the digit so that it fits a square of the digit size we're using for machine learning
 	w = bbox[1][0] - bbox[0][0]
 	h = bbox[1][1] - bbox[0][1]
-
-	# Ignore any small bounding boxes
 	if w > 0 and h > 0 and (w * h) > 100 and len(digit) > 0:
 		return scale_and_centre(digit, size, 4)
 	else:
@@ -227,7 +195,7 @@ def get_digits(img, squares, size):
 		digits.append(extract_digit(img, square, size))
 	return digits
 	
-original = cv2.imread("input.jpeg", cv2.IMREAD_GRAYSCALE)
+original = cv2.imread("input2.jpg", cv2.IMREAD_GRAYSCALE)
 processed = pre_process_image(original)
 corners = find_corners_of_largest_polygon(processed)
 cropped = crop_and_warp(original, corners)
